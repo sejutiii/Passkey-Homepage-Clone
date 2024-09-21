810d5a75b236df922257ca0304854aa11631ebed
sejutiii <126340484+sejutiii@users.noreply.github.com>
2024-09-22 03:58:58 +0600

    passkyyyyy

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html'],
+  theme: {
+    extend: {
+      colors:{
+        base: '#1f2937',
+        btn1: '#374151',
+        base2: '#151c2c',
+        btn2: '#4F46E5',
+        btn2h: '#4338CA',
+      }
+    },
+  },
+  plugins: [],
+}
+
